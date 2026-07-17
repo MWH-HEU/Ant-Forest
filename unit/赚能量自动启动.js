013af91621edf95ff20eb8551c7eb9f6df34db09
MWH-HEU <motangjian@163.com>
2026-07-17 15:28:48 +0800

    赚能量自动启动: 增加40分钟超时机制，锁屏逻辑从子脚本移至父脚本

--- unit/赚能量自动启动.js
+++ unit/赚能量自动启动.js
@@ -3,6 +3,7 @@ let singletonRequire = require('../lib/SingletonRequirer.js')(runtime, global)
 var configStorage = storages.create(_storage_name)
 let FileUtils = singletonRequire('FileUtils')
 let commonFunctions = singletonRequire('CommonFunction')
+let automator = singletonRequire('Automator')
 let { logInfo, errorInfo, warnInfo, debugInfo, infoLog, debugForDev, clearLogFile, flushAllLogs } = singletonRequire('LogUtils')
 config.not_lingering_float_window = true
 if (!commonFunctions.ensureAccessibilityEnabled()) {
@@ -19,13 +20,25 @@ toastLog("配置完毕done")
 commonFunctions.showCommonDialogAndWait('赚能量')
 let mainScriptPath = FileUtils.getRealMainScriptPath(true)
 let childScriptPath = mainScriptPath + "/unit/赚能量.js"
-engines.execScriptFile(childScriptPath, { path: mainScriptPath + "/unit/", arguments: { executeByTimeTask: true, needRelock: unlocker.needRelock() } })
+engines.execScriptFile(childScriptPath, { path: mainScriptPath + "/unit/", arguments: { executeByTimeTask: true } })
 sleep(1000)
 let all = engines.all()
 for (let i = 0; i < all.length; i++) {
   if ((all[i].getSource() + '') === childScriptPath) {
-    while (!all[i].isDestroyed()) sleep(3000)
+    let waitStart = new Date().getTime()
+    while (!all[i].isDestroyed()) {
+      if (new Date().getTime() - waitStart > 40 * 60 * 1000) {
+        debugInfo('子脚本执行超时40分钟，强制退出')
+        break
+      }
+      sleep(3000)
+    }
     break
   }
+}
+// 子脚本执行完毕，锁屏
+if (config.auto_lock === true && unlocker.needRelock() === true) {
+  debugInfo('重新锁定屏幕')
+  automator.lockScreen()
 }
 exit()
